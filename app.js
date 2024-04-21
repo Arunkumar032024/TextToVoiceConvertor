4a4be143efdf0fa47fef937b94878ebcc42ed0c9
Arunkumar032024 <arunkumar032024@gmail.com>
2024-04-21 21:11:15 +0530

    Add js

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 // to access textarea
 const inputText = document.querySelector("textarea");
+
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelect = document.querySelector('select');
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => {
+        (voiceSelect.options[i]) = new Option(voice.name, i);
+    });
+};
+
+
+voiceSelect.addEventListener('change', () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+
+document.querySelector('button').addEventListener('click', () => {
+    speech.text = inputText.value;
+    window.speechSynthesis.speak(speech);
+});
